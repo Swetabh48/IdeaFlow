0fbd4ed97ca9f8a70f52e972a1bd3839728ea464
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-07-13 18:24:17 +0530

    Card actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ConvexClientProvider } from '@/providers/convex-client-provider'
 import { Toaster} from "@/components/ui/sonner"
+import { ModalProvider } from '@/providers/modal-provider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -37,6 +38,7 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ConvexClientProvider>
             <Toaster/>
+            <ModalProvider/>
             {children}
             </ConvexClientProvider>
         </body>
