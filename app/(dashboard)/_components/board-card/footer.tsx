ee8aee6d4c29fe3750119286e7416f4179056dfd
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-07-13 21:51:53 +0530

    Favoriting functionality

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -12,6 +12,15 @@ interface FooterProps{
 
 
 export const Footer=({title,authorLabel,createdAtLabel,isFavorite,onClick,disabled}:FooterProps)=>{
+    
+    const handleClick=(
+        event: React.MouseEvent<HTMLButtonElement,MouseEvent>
+    )=>{
+        event.stopPropagation();
+        event.preventDefault();
+        onClick();
+    }
+    
     return (
         <div className="relative bg-white p-3">
             <p className="text-[13px] truncate max-w-[calc(100%-20px)]">
@@ -22,7 +31,7 @@ export const Footer=({title,authorLabel,createdAtLabel,isFavorite,onClick,disabl
             </p>
             <button
                 disabled={disabled}
-                onClick={onClick}
+                onClick={handleClick}
                 className={cn(
                     "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
                     disabled && "cursor-not-allowed opacity-75"
