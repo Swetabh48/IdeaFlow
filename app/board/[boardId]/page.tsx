a7cd005e11db5a4adf0b0fa4f5e07b220c739a03
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-07-17 11:29:16 +0530

    created room info

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -3,19 +3,18 @@ import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
-    params: {
-        boardId: string;
-    };
-};
+    params: { boardId: string };
+}
 
-const BoardIdPage=({params}:BoardIdPageProps)=>{
+
+const BoardIdPage = ({ params }: BoardIdPageProps)=>{
     
 
-    return(
-        <Room roomId={params.boardId} fallback={<Loading/>}>
-            <Canvas boardId={params.boardId}/>
+   return (
+        <Room roomId={params.boardId} fallback={<Loading />}>
+            <Canvas boardId={params.boardId} />
         </Room>
-    )
+    );
 }
 
 export default BoardIdPage;
