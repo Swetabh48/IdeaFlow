302faeba6ba7807fcccd76683f9fd175a55997e6
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-07-24 10:48:40 +0530

    refactoring code to escape error in deployment

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { use } from "react";
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
 
-export default function DashboardPage({ searchParams }) {
+export default function DashboardPage({ searchParams }: { searchParams: Promise<{ [key: string]: string }> }) {
   const { organization } = useOrganization();
+  const query = use(searchParams);
+
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-5">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={query} />
       )}
     </div>
   );
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,42 +1,14 @@
 // app/board/[boardId]/page.tsx
-
 import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 
-interface BoardIdPageProps {
-  params: { boardId: string };
-}
+export default async function BoardIdPage({ params }: { params: Promise<{ boardId: string }> }) {
+  const { boardId } = await params;
 
-export default async function BoardIdPage({ params }: BoardIdPageProps) {
-  // In Next.js 15+, `params` is a Promise that you properly await:
-  const { boardId } = await Promise.resolve(params); // or await params
   return (
     <Room roomId={boardId} fallback={<Loading />}>
       <Canvas boardId={boardId} />
     </Room>
   );
 }
-
-
-/*"use client";
-
-import { Canvas } from "./_components/canvas";
-import { Room } from "@/components/room";
-import { Loading } from "./_components/loading";
-
-interface BoardIdPageProps {
-    params: { boardId: string };
-}
-
-const BoardIdPage = ({ params }: BoardIdPageProps) => {
-
-
-    return (
-        <Room roomId={params.boardId} fallback={<Loading />}>
-            <Canvas boardId={params.boardId} />
-        </Room>
-    );
-};
-
-export default BoardIdPage;*/
